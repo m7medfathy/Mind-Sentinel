88900396e630d0802dc8309f32b51d57834f73f3
Baher <baherkhedr9@gmail.com>
2026-04-30 23:48:14 +0300

    DbContext Added

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mind_Sentinel.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            var constr = configuration.GetSection("constr").Value;
+
+            optionsBuilder.UseSqlServer(constr);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace Mind_Sentinel
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
-
+            
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
